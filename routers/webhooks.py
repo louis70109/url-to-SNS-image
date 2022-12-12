54295a0388af415d54fbe6618faa51fa648c2ec5
Nijia Lin <louis70109@gmail.com>
2022-12-13 00:38:40 +0800

    💬chore: add temp judgement method

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -70,12 +70,35 @@ def message_text(event):
     image_editable.text((15,15), title_text, (237, 230, 211), font=title_font)
     print('Draw image success.')
 
-    
-    # TODO: resize function
-    resize_image = my_image.resize((int(float(my_image.size[0])*0.3), int(float(my_image.size[1])*0.3)), Image.ANTIALIAS)
+    print('Check and crop image size.')
+    original_w = my_image.size[0]
+    original_h = my_image.size[1]
+    if original_w <= 1080 and original_h <= 1920:
+        # if little than IG size
+        # (540-x/2, 960-y/2)
+        pass
+    elif original_w <= 1080 and original_h > 1920:
+        # if only heigh more than IG heigh
+        # (540-(x/2), (y/2)-960)
+        pass
+    elif original_w > 1080 and original_h > 1920:
+        # if only width more tha IG width
+        # ((x/2)-540, 960-(y/2))
+        pass
+    else:
+        # if width and height more than IG size.
+        # ((x/2)-540, (y/2)-960)
+        pass
+
+    # TODO: resize function, 
     print('Resize done.')
+    w_rate = my_image.size[0]
+    h_rate = my_image.size[1]
+    resize_image = my_image.resize((int(float(my_image.size[0])*0.3), int(float(my_image.size[1])*0.3)), Image.ANTIALIAS)
+    
+    # Put size into this line
     new_image = Image.new('RGB',(1080, 1920), (250,250,250))
-    image1 = Image.open('white_ig.jpeg') # Change to no background PNG
+    image1 = Image.open('white_ig.png') # Change to no background PNG
     new_image.paste(image1,(0,0))
     new_image.paste(resize_image,(200,720))
     new_image.save("merged_image.jpg","JPEG")
